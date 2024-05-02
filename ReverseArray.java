1e20c6a799494f71defb5130ee5f4a623edf58d6
eduardoevz <165875920+eduardoevz@users.noreply.github.com>
2024-05-01 20:27:58 -0600

    Add files via upload

---
+++ ReverseArray.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReverseArray {
+
+    public static void main(String[] args) {
+        int[] Arreglo = new int[5];
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("introduce 5 numeros diferentes");
+
+        for (int i = 0; i < Arreglo.length; i++) {
+            try {
+                Arreglo[i] = scanner.nextInt();
+            } catch (Exception e) {
+                System.out.println("no valido, introduce un numero ");
+                i--;
+                scanner.next();
+            }
+        }
+
+        System.out.println("el arreglo original es " + Arrays.toString(Arreglo));
+        System.out.println("el arreglo invertido es " + Arrays.toString(reverseArray(Arreglo)));
+    }
+
+    public static int[] reverseArray(int[] arr) {
+        int[] ArregloReverso = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            ArregloReverso[arr.length - 1 - i] = arr[i];
+        }
+        return ArregloReverso;
+    }
+}
